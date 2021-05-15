53a9c18eb0aa5e733a1e304ed643d9c287be7977
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2021-05-15 17:15:50 +0200

    added BeeMovieScript.dart

---
+++ dart/example.dart
@@ -0,0 +1,14 @@
+import "BeeMovieScript.dart";
+
+import "dart:core";
+
+void main() {
+
+  // prints out the whole Bee Movie script
+  print(BeeMovieScriptText);
+
+  // prints out the whole Bee Movie script
+  for (int index = 0; index < BeeMovieScriptList.length; index++) {
+    print(BeeMovieScriptList[index]);
+  }
+}
